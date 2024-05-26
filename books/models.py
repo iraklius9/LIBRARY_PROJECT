a3c56fa54f80172bc72f8bc7345c74d1f3ec44f6
iraklius9 <iraklius123456789@gmail.com>
2024-05-26 14:23:33 +0400

    added gettext_lazy

--- books/models.py
+++ books/models.py
@@ -1,29 +1,38 @@
 from django.db import models
 from django.conf import settings
 from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
 
 
 class Genre(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(_("Name"), max_length=100)
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        verbose_name = _("Genre")
+        verbose_name_plural = _("Genres")
 
 
 class Author(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(_("Name"), max_length=100)
 
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name = _("Author")
+        verbose_name_plural = _("Authors")
+
 
 class Book(models.Model):
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)
-    genre = models.ManyToManyField(Genre)
-    title = models.CharField(max_length=200)
-    publication_date = models.DateField()
-    stock_quantity = models.IntegerField()
-    image = models.ImageField(upload_to='book_images', null=True, blank=True, default='book_images/2.jpg')
+    author = models.ForeignKey(Author, verbose_name=_("Author"), on_delete=models.CASCADE)
+    genre = models.ManyToManyField(Genre, verbose_name=_("Genre"))
+    title = models.CharField(_("Title"), max_length=200)
+    publication_date = models.DateField(_("Publication Date"))
+    stock_quantity = models.IntegerField(_("Stock Quantity"))
+    image = models.ImageField(_("Image"), upload_to='book_images', null=True, blank=True, default='book_images/2.jpg')
 
     def __str__(self):
         return self.title
@@ -41,44 +50,59 @@ class Book(models.Model):
     def reserved_quantity(self):
         return Reservation.objects.filter(book=self).count()
 
+    class Meta:
+        verbose_name = _("Book")
+        verbose_name_plural = _("Books")
+
 
 class BookInstance(models.Model):
     STATUS_CHOICES = [
-
-        ('On loan', 'On loan'),
-        ('Maintenance', 'Maintenance'),
-        ('Reserved', 'Reserved'),
-        ('Returned', 'Returned'),
+        ('On loan', _('On loan')),
+        ('Maintenance', _('Maintenance')),
+        ('Reserved', _('Reserved')),
+        ('Returned', _('Returned')),
     ]
 
-    book = models.ForeignKey(Book, on_delete=models.CASCADE)
-    borrower = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True)
-    borrowed_date = models.DateField(null=False, blank=False)
-    returned_date = models.DateField(null=False, blank=False)
-    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='On loan')
+    book = models.ForeignKey(Book, verbose_name=_("Book"), on_delete=models.CASCADE)
+    borrower = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name=_("Borrower"), on_delete=models.SET_NULL,
+                                 null=True, blank=True)
+    borrowed_date = models.DateField(_("Borrowed Date"), null=False, blank=False)
+    returned_date = models.DateField(_("Returned Date"), null=False, blank=False)
+    status = models.CharField(_("Status"), max_length=20, choices=STATUS_CHOICES, default='On loan')
 
     def __str__(self):
         return f'{self.book.title}'
 
+    class Meta:
+        verbose_name = _("Book Instance")
+        verbose_name_plural = _("Book Instances")
+
 
 class Reservation(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    book = models.ForeignKey(Book, on_delete=models.CASCADE)
-    reserved_at = models.DateTimeField(auto_now_add=True)
-    expires_at = models.DateTimeField()
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name=_("User"), on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, verbose_name=_("Book"), on_delete=models.CASCADE)
+    reserved_at = models.DateTimeField(_("Reserved At"), auto_now_add=True)
+    expires_at = models.DateTimeField(_("Expires At"))
 
     @property
     def is_expired(self):
         return self.expires_at < timezone.now()
 
+    class Meta:
+        verbose_name = _("Reservation")
+        verbose_name_plural = _("Reservations")
+
 
 class BorrowingHistory(models.Model):
-    book_instance = models.ForeignKey(BookInstance, on_delete=models.CASCADE)
-    book = models.ForeignKey(Book, on_delete=models.CASCADE)
-    borrower = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    borrowing_date = models.DateTimeField()
-    returning_date = models.DateTimeField(null=True, blank=True)
+    book_instance = models.ForeignKey(BookInstance, verbose_name=_("Book Instance"), on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, verbose_name=_("Book"), on_delete=models.CASCADE)
+    borrower = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name=_("Borrower"), on_delete=models.CASCADE)
+    borrowing_date = models.DateTimeField(_("Borrowing Date"))
+    returning_date = models.DateTimeField(_("Returning Date"), null=True, blank=True)
 
+    def __str__(self):
+        return f"{self.book.title} borrowed by {self.borrower.username} on {self.borrowing_date}"
 
-def __str__(self):
-    return f"{self.book.title} borrowed by {self.borrower.username} on {self.borrowing_date}"
+    class Meta:
+        verbose_name = _("Borrowing History")
+        verbose_name_plural = _("Borrowing Histories")
--- users/models.py
+++ users/models.py
@@ -8,14 +8,15 @@ class CustomUser(AbstractUser):
     email = models.EmailField(_('email address'), unique=True)
     full_name = models.CharField(_('full name'), max_length=150)
     personal_number = models.CharField(
+        _('personal number'),
         max_length=11,
         unique=True,
-        validators=[RegexValidator(r'^\d{11}$', 'Personal number must be 11 digits.')]
+        validators=[RegexValidator(r'^\d{11}$', _('Personal number must be 11 digits.'))]
     )
-    birth_date = models.DateField(null=True, blank=False)
+    birth_date = models.DateField(_('birth date'), null=True, blank=False)
 
-    is_staff = models.BooleanField(default=False)
-    is_user = models.BooleanField(default=False)
+    is_staff = models.BooleanField(_('staff status'), default=False)
+    is_user = models.BooleanField(_('user status'), default=False)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username', 'full_name', 'birth_date']
@@ -29,16 +30,19 @@ class CustomUser(AbstractUser):
         Group,
         related_name='custom_users',
         blank=True,
-        help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.'
+        help_text=_(
+            'The groups this user belongs to. A user will get all permissions granted to each of their groups.'),
+        verbose_name=_('groups')
     )
 
     user_permissions = models.ManyToManyField(
         Permission,
         related_name='custom_users',
         blank=True,
-        help_text='Specific permissions for this user.'
+        help_text=_('Specific permissions for this user.'),
+        verbose_name=_('user permissions')
     )
 
     class Meta:
-        verbose_name = 'User'
-        verbose_name_plural = 'Users'
+        verbose_name = _('User')
+        verbose_name_plural = _('Users')
